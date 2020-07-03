4a4672738e25853e8f2c7560d4499e8c32a15434
Ronald Mitterer <info@abytemore.com>
2020-07-03 17:09:50 +0200

    initial commit

--- src/ComposerHelper.php
+++ src/ComposerHelper.php
@@ -1,10 +1,52 @@
 <?php
 
-namespace abytemore;
+namespace abytemore\ComposerHelper;
+
+use Composer\Installer\InstallerEvent;
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
 
 class ComposerHelper
 {
-    public function test() {
-        echo "Hello World!";
+    public static function preUpdate(Event $event)
+    {
+        $composer = $event->getComposer();
+        // do stuff
+    }
+
+    public static function postUpdate(Event $event)
+    {
+        $composer = $event->getComposer();
+        // do stuff
+    }
+
+    public static function preInstall(InstallerEvent $event)
+    {
+        $composer = $event->getComposer();
+        // do stuff
+    }
+    public static function postInstall(InstallerEvent $event)
+    {
+        $composer = $event->getComposer();
+        // do stuff
+    }
+
+    public static function postAutoloadDump(Event $event)
+    {
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        require $vendorDir . '/autoload.php';
+
+//        some_function_from_an_autoloaded_file();
+    }
+
+    public static function postPackageInstall(PackageEvent $event)
+    {
+        $installedPackage = $event->getOperation()->getPackage();
+        // do stuff
+    }
+
+    public static function warmCache(Event $event)
+    {
+        // make cache toasty
     }
 }
